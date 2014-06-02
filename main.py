d5188d1200fbd1a252a484a3cc7768e082027fd9
Ben Thompson <t.ben.thompson@gmail.com>
2014-06-02 09:55:40 -0400

    Initial commit. Testing the basic building blocks.

---
+++ main.py
@@ -0,0 +1,94 @@
+from collections import namedtuple
+from functools import partial
+
+Branch = namedtuple('Branch', ['name', 'args'])
+Rule = namedtuple('Rule', ['lhs', 'rhs'])
+
+Minus = partial(Branch, '-')
+Plus = partial(Branch, '+')
+Mult = partial(Branch, '*')
+Div = partial(Branch, '/')
+
+zero = '0'
+one = '1'
+any_identifier = '__any'
+any = lambda name: any_identifier + str(name)
+
+rules = []
+rules.append(Rule(Minus([any(0), zero]), any(0)))
+
+class TransformException(Exception): pass
+
+# def traverse_args(expr, rules):
+#     if type(expr) is str:
+#         return expr
+#     for idx, a in enumerate(expr.args):
+#         orig_expr = expr.args[idx]
+#         expr.args[idx] = apply_rules(orig_expr, rules)
+#     return expr
+
+
+def apply_rules(expr, rules):
+    done = False
+    while not done:
+        orig_expr = expr
+        expr = match_and_transform(orig_expr, rules)
+        # expr = traverse_args(expr, rules)
+        if expr is orig_expr:
+            done = True
+
+
+def match_vars(var, template):
+    split_template_name = template.split(any_identifier)
+    if len(split_template_name) == 2:
+        return True, {split_template_name[1]: var}
+    if var != template:
+        return False, {}
+    return True, {}
+
+
+def match_args(expr, template):
+    if len(expr.args) != len(template.args):
+        return False, {}
+    any_list = dict()
+    for expr_arg, template_arg in zip(expr.args, template.args):
+        is_match, more_any = match(expr_arg, template_arg)
+        if not is_match:
+            return False, {}
+        any_list.update(more_any)
+    return True, any_list
+
+
+def match(expr, template):
+    if type(expr) is str:
+        return match_vars(expr, template)
+    if expr.name != template.name:
+        return False, {}
+    return match_args(expr, template)
+
+
+def transform_var(template, any_list):
+    split_template_name = template.split(any_identifier)
+    if len(split_template_name) != 2:
+        return template
+    if split_template_name[1] not in any_list:
+        raise TransformException("Transforming improperly matched expression")
+    return any_list[split_template_name[1]]
+
+def transform(template, any_list):
+    if type(template) is str:
+        return transform_var(template, any_list)
+    new_args = []
+    for template_arg in template.args:
+        new_args.append(transform(template_arg, any_list))
+    return Branch(template.name, new_args)
+
+
+def match_and_transform(expr, rules):
+    for r in rules:
+        is_match, any_list = match(expr, r.lhs)
+        if not is_match:
+            continue
+        is_match, any_list = match_args(r.lhs.args, expr.args)
+    #     new_expr = r.rhs
+    # return expr
---
+++ test.py
@@ -0,0 +1,72 @@
+import pytest
+from main import *
+
+
+# MATCHING TESTS
+def test_match_vars_true():
+    assert(match_vars('a', 'a') == (True, {}))
+
+
+def test_match_vars_false():
+    assert(match_vars('a', 'b') == (False, {}))
+
+
+def test_match_vars_any():
+    assert(match_vars('a', any(1)) == (True, {'1': 'a'}))
+
+
+def test_match_simple():
+    expr = Branch('my_name', [])
+    template = Branch('my_name2', [])
+    assert(match(expr, template) == (False, {}))
+
+
+def test_match_simple2():
+    expr = Branch('my_name', [])
+    template = Branch('my_name', [])
+    assert(match(expr, template) == (True, {}))
+
+
+def test_match_minus():
+    expr = Minus(['x', zero])
+    template = Minus([any(1), zero])
+    assert(match(expr, template) == (True, {'1': 'x'}))
+
+
+def test_match_deeper():
+    expr = Minus([Minus(['x', zero]), 'x'])
+    template = Minus([Minus([any(1), zero]), any(2)])
+    assert(match(expr, template) == (True, {'1': 'x', '2': 'x'}))
+
+
+def test_match_deeper2():
+    expr = Minus([Minus(['x', zero]), 'x'])
+    template = Minus([Minus([any(1), zero]), 'y'])
+    assert(match(expr, template) == (False, {}))
+
+
+# REPLACEMENT TESTS
+def test_transform0():
+    assert(transform(any(0), {'0': 'y'}) == 'y')
+
+
+def test_transform1():
+    template = Minus([any(0), '0'])
+    assert(transform(template, {'0': 'x'}) == Minus(['x', '0']))
+
+
+def test_transform2():
+    template = Minus([any(0), any(10)])
+    with pytest.raises(TransformException):
+        transform(template, {'0': 'x'}) == Minus(['x', '0'])
+
+
+def test_match_transform_identity():
+    expr = Minus([Minus(['x', zero]), 'x'])
+    template = Minus([Minus([any(1), zero]), any(2)])
+    matches, any_list = match(expr, template)
+    result = transform(template, any_list)
+    assert(match(result, expr) == (True, {}))
+
+# APPLY RULES
+
